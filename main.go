3ea8d48082deab15d38125dabd02a1a0a7673b27
Marcus Bello <marcusthetechguru@gmail.com>
2025-07-29 04:16:32 -0500

    WIP: added init() and command line flags

--- main.go
+++ main.go
@@ -1,6 +1,34 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+const (
+	DefaultHTTPAddr = "localhost:8080"
+)
+
+var numberOfWorkers int = 3
+var httpAddr string
+
+func init() {
+	flag.Int("workers", numberOfWorkers, "Number of worker goroutines")
+	flag.StringVar(&httpAddr, "haddr", DefaultHTTPAddr, "Set the HTTP address")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [options] \n", os.Args[0])
+		flag.PrintDefaults()
+	}
+}
+
 
 func main() {
-	
+	flag.Parse()
+	if len(flag.Args()) != 1 {
+		flag.Usage()
+		log.Printf("Using default HTTP address: %s with %d Workers ", httpAddr, numberOfWorkers)
+	}
+
 }
